32b6897784ad9eed13942b5b7f2ca471019cefd4
Icarus <samalexson1214@gmail.com>
2026-09-03 18:36:44 +0800

    Fix formatting of PDO error mode setting

--- config/db.php
+++ config/db.php
@@ -6,7 +6,7 @@ $password = '';
 // pdo = PHP data objects - connect to MySQL Database
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE,            PDO::ERRMODE_EXCEPTION); // show errors
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // show errors
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); // fetch data as associative arrays
 } catch (PDOException $e) {
     die("Failed to connect to database: " . $e->getMessage());
